77a2ef8d7922d58f89ca6d03d9ab3b60b4e2704d
Jesse Friberg <jesse.friberg@edu.turkuamk.fi>
2025-07-01 17:17:26 +0300

    Stack

--- Spellbinder/app/index.jsx
+++ Spellbinder/app/index.jsx
@@ -1,12 +1,16 @@
 import { View, Text, StyleSheet, Image } from "react-native";
 import spellbinderLogo from "@/assets/images/Spellbinder_logo.png";
 import React from "react";
+import { Link } from "expo-router";
 
 const app = () => {
   return (
     <View style={styles.container}>
       <Image source={spellbinderLogo} style={styles.image} resizeMode="contain"></Image>
       <Text style={styles.text}>Lorem ipsum</Text>
+      <Link href="/explore" style={styles.linkText}>
+        Explore
+      </Link>
     </View>
   );
 };
@@ -24,11 +28,19 @@ const styles = StyleSheet.create({
     fontSize: 40,
     fontWeight: "bold",
     textAlign: "center",
+    marginBottom: 100,
   },
   image: {
     maxWidth: "95%",
     maxHeight: "25%",
     // backgroundColor: "rgba(255,255,255,0.5)",
     alignSelf: "center",
+  },
+  linkText: {
+    color: "white",
+    fontSize: 40,
+    fontWeight: "bold",
+    textAlign: "center",
+    textDecorationLine: "underline",
   },
 });
